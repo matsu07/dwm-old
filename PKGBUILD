e96d6640df9df6e6d420d385c5bc2d160bee5964
Kelvin <eikiti.matsumoto@gmail.com>
2015-09-22 21:18:42 -0300

    Added patches, fixed PKGBUILD, patch 14 needs fixing, dwm is now functional

--- PKGBUILD
+++ PKGBUILD
@@ -11,12 +11,21 @@ install=dwm.install
 source=(http://dl.suckless.org/dwm/dwm-$pkgver.tar.gz
 	dwm.desktop
 	config.h
-  	gaplessgrid.c)
+  	gaplessgrid.c
+	maximize.c)
 
-_patches=(01-6.0-pango.diff
-	  02-6.0-maximize.diff
-	  03-6.0-swapfocus.diff
-	  04-6.0-pertag.diff)
+_patches=(00-dwm-6.0-buildflags.diff
+	  01-dwm-6.0-xft.diff
+	  02-dwm-6.0-pertag2.diff
+	  03-dwm-6.0-betterborders.diff
+	  04-dwm-6.0-systray.diff
+	  05-dwm-6.0-statuscolors.diff
+	  06-dwm-6.0-monocle_fixes.diff
+	  07-dwm-6.0-maximize.diff
+	  08-dwm-6.0-swapfocus.diff
+	  09-dwm-6.0-cycle.diff
+	  10-dwm-6.0-moveresize.diff
+	  12-dwm-6.0-XKeycodeToKeysym_fix.diff)
 
 source=(${source[@]} ${_patches[@]})
 
@@ -31,6 +40,7 @@ build() {
 
   cp $srcdir/config.h config.h
   cp $srcdir/gaplessgrid.c gaplessgrid.c
+  cp $srcdir/maximize.c maximize.c
 
   sed -i 's/CPPFLAGS =/CPPFLAGS +=/g' config.mk
   sed -i 's/^CFLAGS = -g/#CFLAGS += -g/g' config.mk
@@ -49,9 +59,18 @@ package() {
 }
 md5sums=('8bb00d4142259beb11e13473b81c0857'
          '939f403a71b6e85261d09fc3412269ee'
-         '39fbfaa423ebc40b1ecb92eecf4ea638'
+         'b92ff1d2bd2fa484e35b462ce509ba31'
          '4ba509b3b93f7b1418dc703c70de536f'
-         '983aa266382db7e0e05e93617f33024e'
-         '7a598c9fbb18bac61faab67a3fe42a86'
-         'd4c301f72ea9d07b443f6cc44a6de551'
-         '7f7580e8b77e6f77f27a7bb009173757')
+         '015213049aa1fdf6d489348d44df05df'
+         '09e3586f19d5d5d59379bab3d8d512ff'
+         'ce52e9da51fea4d44e217441b1159452'
+         '65c410a0fae5704a0dcba8a76f86ced1'
+         '3f038ce43e1e6632ee962cb6a2478136'
+         '77e623c7211a00649cba6d100557d772'
+         '2ab833f04ac52857d876140e6417141e'
+         '7e2d6ad94e096605e9158fbcfe0bfbec'
+         '13cb3f721439f9f3b44be88c6341c957'
+         'dba56f36c523ed6cb76b1ade4d400b9e'
+         'd9531d4676cfef402776da39a9882402'
+         '8c39ec40770167f56ccf4f8f5af33dbb'
+         'd326f2df4f1a665f1fdf72afc424e69a')
--- config.h
+++ config.h
@@ -1,21 +1,46 @@
 /* See LICENSE file for copyright and license details. */
 
-/* appearance */
-static const char font[]            = "Sans 8";
-static const char normbordercolor[] = "#444444";
-static const char normbgcolor[]     = "#222222";
-static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#005577";
-static const char selbgcolor[]      = "#005577";
-static const char selfgcolor[]      = "#eeeeee";
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const Bool showbar           = True;     /* False means no bar */
-static const Bool topbar            = True;     /* False means bottom bar */
-
-/* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-
+/* Appearance */
+#define NUMCOLORS 5
+static const char colors[NUMCOLORS][ColLast][20] = {
+    // border     fg         bg
+    { "#ababab", "#cacaca", "#020202" },  // 01 - normal
+    { "#AF7130", "#20b2e7", "#020202" },  // 02 - selected
+    { "#B3354C", "#B3354C", "#020202" },  // 03 - urgent
+    { "#118900", "#FFB950", "#020202" },  // 04 - orange (Occupied Color)
+    { "#20b2e7", "#20b2e7", "#020202" },  // 05 - Light Blue
+//    { "#608040", "#608040", "#020202" },  // 06 - green
+//    { "#877C43", "#877C43", "#020202" },  // 07 - yellow
+//    { "#1C678C", "#1C678C", "#020202" },  // 08 - blue
+//    { "#E300FF", "#E300FF", "#020202" },  // 09 - magenta
+//    { "#000000", "#000000", "#000000" },  // unusable
+//    { "#337373", "#337373", "#020202" },  // 0B - cyan
+//    { "#808080", "#808080", "#020202" },  // 0C - light gray
+//    { "#4C4C4C", "#4C4C4C", "#020202" },  // 0D - gray
+//    { "#FFEE00", "#FFEE00", "#020202" },  // 0E - yellow2
+//    { "#B1D354", "#B1D354", "#020202" },  // 0F - light green 
+//    { "#BF9F5F", "#BF9F5F", "#020202" },  // 10 - light yellow
+//    { "#3995BF", "#3995BF", "#020202" },  // 11 - light blue
+//    { "#A64286", "#A64286", "#020202" },  // 12 - light magenta
+//    { "#6C98A6", "#6C98A6", "#020202" },  // 13 - light cyan
+//    { "#FFA500", "#FFA500", "#020202" },  // 14 - orange
+//
+//    { "#0300ff", "#0300ff", "#802635" },  // 15 - warning
+};
+  
+static const char font[]                 = "Anonymous Pro 8";
+/*static const char normbordercolor[]      = "#444444";
+static const char normbgcolor[]          = "#222222";
+static const char normfgcolor[]          = "#bbbbbb";
+static const char selbordercolor[]       = "#005577";
+static const char selbgcolor[]           = "#005577";
+static const char selfgcolor[]           = "#eeeeee";*/
+static const unsigned int borderpx       = 1;        /* border pixel of window */
+static const unsigned int snap           = 32;       /* snap pixel */
+static const Bool showbar                = True;     /* False means no bar */
+static const Bool topbar                 = True;     /* False means bottom bar */
+static const unsigned int systrayspacing = 2;
+static const Bool showsystray            = True;
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
@@ -27,6 +52,7 @@ static const float mfact      = 0.55; /* factor of master area size [0.05..0.95]
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
 
+#include "maximize.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
@@ -34,8 +60,19 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
+/* tagging */
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const Tag tags[] = {
+/* name    	layout       	mfact  	nmaster*/
+    { "A",   	&layouts[0], 	-1,    	-1 },
+    { "B",	&layouts[0], 	-1,  	-1 },
+    { "C",  	&layouts[0], 	-1,    	-1 },
+    { "D", 	&layouts[0], 	-1,    	-1 },
+    { "E",  	&layouts[0], 	-1,    	-1 },
+};
+
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -46,8 +83,9 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "uxterm", NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL};
+static const char *termcmd[]  = { "st", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -84,6 +122,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_h,      togglehorizontalmax, NULL},
+	{ MODKEY|ControlMask|ShiftMask, XK_l,      togglehorizontalmax, NULL },
+	{ MODKEY|ControlMask|ShiftMask, XK_j,      toggleverticalmax,   NULL },
+	{ MODKEY|ControlMask|ShiftMask, XK_k,      toggleverticalmax,   NULL },
+	{ MODKEY|ControlMask,           XK_m,      togglemaximize,      {0} },
 };
 
 /* button definitions */
